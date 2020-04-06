2a3e54e73696ee4601e913f3be4b46357d780df0
Ajeet Chauhan <ajeet.chauhan@ucertify.com>
2020-04-06 09:08:29 +0530

    Chnaes

--- lib/main.dart
+++ lib/main.dart
@@ -48,6 +48,7 @@ class StartQuiz extends StatelessWidget {
             child: Column(
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
+            firstScreen(),
             new Container(
               padding: new EdgeInsets.only(top: 16.0),
               child: new Row(
@@ -90,5 +91,87 @@ class StartQuiz extends StatelessWidget {
             )
           ],
         )));
+  }
+}
+
+class firstScreen extends StatelessWidget {
+  String removeHour(String htmlText) {
+    RegExp exp = RegExp(r"hours|hour", multiLine: true, caseSensitive: true);
+    RegExp min =
+        RegExp(r"minutes|minute", multiLine: true, caseSensitive: true);
+    htmlText = htmlText.replaceAll(exp, 'h');
+    return htmlText.replaceAll(min, 'm');
+  }
+
+  Widget build(BuildContext context) {
+    final appState = Provider.of<AppState>(context);
+    //final fetchStat = appState.fetchStat;
+    final masteredItems = appState.masteredItems;
+    final totalTime = appState.totalTime;
+    final getResult = appState.getResult;
+    final masteredItemsPerc = appState.masteredItemsPerc;
+    final inPlay = appState.inPlay;
+    final inPlayPerc = appState.inPlayPerc;
+    final pendingItems = appState.pendingItems;
+    final pendingItemsPerc = appState.pendingItemsPerc;
+    final showDraw = appState.showDraw;
+    getResult();
+    return showDraw
+        ? Container(
+            padding: EdgeInsets.all(20.00),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    child: Column(children: <Widget>[
+                      Icon(Icons.check_circle, color: Colors.green, size: 40.0),
+                      Text("Mastered",
+                          style: new TextStyle(fontWeight: FontWeight.bold)),
+                      Text(masteredItems + "(" + masteredItemsPerc + "%)")
+                    ]),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    child: Column(children: <Widget>[
+                      Icon(Icons.play_circle_filled,
+                          color: Colors.orange, size: 40.0),
+                      Text("In Play",
+                          style: new TextStyle(fontWeight: FontWeight.bold)),
+                      Text(inPlay + "(" + inPlayPerc + "%)")
+                    ]),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    child: Column(children: <Widget>[
+                      Icon(CupertinoIcons.ellipsis,
+                          color: Colors.red, size: 40.0),
+                      Text("Pending",
+                          style: new TextStyle(fontWeight: FontWeight.bold)),
+                      Text(pendingItems + "(" + pendingItemsPerc + "%)")
+                    ]),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    child: Column(children: <Widget>[
+                      Icon(
+                        CupertinoIcons.time_solid,
+                        color: Colors.blueAccent,
+                        size: 40.0,
+                      ),
+                      Text("Time",
+                          style: new TextStyle(fontWeight: FontWeight.bold)),
+                      Text(removeHour(totalTime))
+                    ]),
+                  ),
+                ),
+              ],
+            ),
+          )
+        : Center(
+            child: CircularProgressIndicator(),
+          );
   }
 }
